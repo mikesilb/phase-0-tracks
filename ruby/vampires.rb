e61949897116b1bf9b94b6a9b3ddfb0c5b9a993d
Sean Kung <RealNumbers@RealNumbers-MBP.local>
2016-09-14 11:50:57 -0500

    Adds data gathering to vampires.rb

---
+++ ruby/vampires.rb
@@ -0,0 +1,23 @@
+# write a program that asks the new employee the following questions.
+#What is your name? Some known vampires are in the area, and we can check against the Werewolf Intelligence Bureau database for their aliases.
+
+#How old are you? What year were you born? This is to try to trick the vampire, who is likely several hundreds of years old. If an employee gives an age and a year of birth that don’t line up mathematically, that employee might be a vampire.
+
+#Our company cafeteria serves garlic bread. Should we order some for you? Vampires hate garlic. Do not even get a vampire started on garlic. Invite a vampire to an Italian restaurant even one time, and you’ll never hear the end of it.
+
+#Would you like to enroll in the company’s health insurance? Vampires are immortal, so they certainly don’t need health insurance.
+
+puts ("What is your name?")
+name = gets.chomp
+
+puts ("How old are you?")
+age = gets.chomp
+
+puts ("What year were you born?")
+year = gets.chomp
+
+puts ("Our company cafeteria serves garlic bread. Should we order some for you?")
+garlic = gets.chomp
+
+puts ("Would you like to enroll in the company’s health insurance?")
+insurance = gets.chomp
