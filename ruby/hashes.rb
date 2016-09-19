df50882cf4292fdea0d6c1a1eaf8fe5113d8bc66
Sean Kung <RealNumbers@RealNumbers-MBP.local>
2016-09-19 14:25:22 -0500

    Adds prompts, defines to_bool function

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -10,3 +10,32 @@
 	reprint the new hash and exit
 
 =end
+#initialize hash
+client_info = {
+	:name => "",
+	:age =>0,
+	:number_of_children => 0,
+	:decor_theme => "",
+	:paying_with_cash => false
+}
+
+def to_bool(str)
+	str =="true"
+end
+
+puts "What is the client's name"
+client_info[:name] = gets.chomp
+
+puts "What is the client's age?"
+client_info[:age] = gets.chomp.to_i
+
+puts "How many children does the client have?"
+client_info[:number_of_children] = gets.chomp.to_i
+
+puts "What is their preferred decor theme?"
+client_info[:decor_theme] = gets.chomp.to_i
+
+puts "Will they be paying in cash? (true/false)"
+client_info[:paying_with_cash] = to_bool(gets.chomp)
+
+puts client_info
