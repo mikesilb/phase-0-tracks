33f9305c6340c1bdef8056a723d73147a6684031
Sean Kung <RealNumbers@RealNumbers-MBP.local>
2016-09-19 14:14:31 -0500

    Adds pseudocode in a comment

---
+++ ruby/hashes.rb
@@ -0,0 +1,12 @@
+=begin
+	Create hash and name all data types
+	prompt user for all the information (the client's name, age, number of children,
+	decor theme, paying with cash)
+	use gets.chomp to save the data into a hash and convert it to the appropriate data type. 
+	Once all questions are answered print out the hash
+	promt the user if they want to make any changes
+		if they enter the data type as a string it should prompt the user to re-enter the data
+		since we're not looping the user can only fix one data value
+	reprint the new hash and exit
+
+=end
