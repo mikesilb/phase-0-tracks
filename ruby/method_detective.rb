308403cf3029ee874c669d50a5a2ec300c3bb4f2
Sean Kung <RealNumbers@RealNumbers-MBP.local>
2016-09-19 13:17:12 -0500

    Does all the detective work

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,37 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+"iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+"zom".gsub(/[o]/, "oo")
 # => “zoom”
 
-# "enhance".<???>
+"enhance".center(19)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+"Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
-#=> "the usual suspects"
-
-# " suspects".<???>
+"the usual".ljust(18, " suspects")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+" suspects".rjust(18, "the usual")
+# => "the usual suspects"
+
+"The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+"The mystery of the missing first letter".delete "T"
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+"Elementary,    my   dear        Watson!".squeeze(' ')
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+"z".each_byte {|c| print c, ' '}
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
+# It is the printable ascii code for "z"
 
-# "How many times does the letter 'a' appear in this string?".<???>
+"How many times does the letter 'a' appear in this string?".count "a"
 # => 4
