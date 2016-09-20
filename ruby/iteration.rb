b58646c0a454fad37370968f9160a4d67c56edaa
Sean Kung <RealNumbers@RealNumbers-MBP.local>
2016-09-20 16:11:02 -0500

    Adds comments for clear understanding

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -21,10 +21,13 @@ seasons.each do |name value|
 end
 
 arr.map! {|name| puts "#{name} is cool"}
-# release 2
 
 seasons.map { |name, value| puts "#{name}'s favorite season is #{value}"}
+# release 2
 
+# Reinitialize arr and seasons hash after each method call so that the next method can
+# be called effectively.
+# Question 1
 arr = [ 10, 2, 6, 15, 23, 4, 1]
 arr.delete_if { |number| number < 5}
 
@@ -36,6 +39,7 @@ seasons = {
 }
 seasons.delete_if {|name, value| value == "fall"}
 
+# Question 2
 arr = [ 10, 2, 6, 15, 23, 4, 1]
 arr.keep_if { |number| number < 5}
 
@@ -47,6 +51,7 @@ seasons = {
 }
 seasons.keep_if {|name, value| value == "fall" || value == "winter"}
 
+# Question 3
 arr = [ 10, 2, 6, 15, 23, 4, 1]
 arr.sort! { |number1, number2| number2 <=> number1 }
 
@@ -58,6 +63,7 @@ seasons = {
 }
 seasons.sort {|name1, name2| name1 <=> name2 }
 
+# Question 4
 arr = [ 10, 2, 6, 15, 23, 4, 1]
 arr.drop_while { |number| number > 5}
 
