330b4f3f0b80baea143bb0449d4f1c22ec294f3a
Sean Kung <RealNumbers@RealNumbers-MBP.local>
2016-09-26 16:34:09 -0500

    Adds Santa class, adds init and 2 methods

---
+++ ruby/santa.rb
@@ -0,0 +1,14 @@
+class Santa
+	def initialize
+		p "Initializing Santa instance..."
+	end
+
+	def speak
+		p "Ho, ho ho! Happy Holidays!"
+	end
+
+	def eat_milk_and_cookies(cookie)
+		p "That was a good #{cookie}!"
+	end
+end
+
