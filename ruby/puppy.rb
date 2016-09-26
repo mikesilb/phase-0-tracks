4284ae84d828365e4e0a6332c7e9808ccddd692d
Sean Kung <RealNumbers@RealNumbers-MBP.local>
2016-09-26 13:18:36 -0500

    Adds puppy class pseudocode

---
+++ ruby/puppy.rb
@@ -0,0 +1,21 @@
+# 6.1 Classes
+=begin 
+PUPPY CLASS ------------------------
+Adorable Puppies!
+
+CHARACTERISTICS ----------------
+Hair Length: varies
+Friendly: varies
+Eye count: 2
+Name: varies
+Color: varies
+Trained: varies
+Adorable: Yes
+
+BEHAVIOR -----------------------
+Begging
+Eating
+Barking
+Pooping
+
+=end
