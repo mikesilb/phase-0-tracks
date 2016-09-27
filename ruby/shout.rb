9eb84a4a5415d6eab4480e5ee5f0a1527877729a
Sean Kung <RealNumbers@RealNumbers-MBP.local>
2016-09-27 13:43:49 -0500

    Removes self from module and creates two classes that include Shout

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,17 +1,32 @@
 # 6.4 
 module Shout
   # we'll put some methods here soon, but this code is fine for now!
-  def self.yell_angrily(words)
+  def yell_angrily(words)
     words + "!!!" + " :("
   end
 
-  def self.yell_happily(words)
+  def yell_happily(words)
   	words + "!!!" + " :)"
   end
 end
-
+=begin
 # DRIVER CODE
 p Shout.yell_happily("I'm Happy")
 # should return "I'm Happy!!! :)"
 p Shout.yell_angrily("I'm ANGRY")
 # should return "I'm ANGRY!!! :("
+=end
+class Dragonborn
+	include Shout
+end
+
+class Children
+	include Shout
+end
+
+dragonborn = Dragonborn.new
+child = Children.new
+
+# DRIVER CODE
+p dragonborn.yell_angrily("Fus Ro Dah")
+p child.yell_happily("It's so fluffy i'm going to die")
