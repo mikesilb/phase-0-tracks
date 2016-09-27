f5aee4c4edcdd717a181c550540cb6ac8f3475ae
Sean Kung <RealNumbers@RealNumbers-MBP.local>
2016-09-26 19:28:31 -0500

    Adds getters and setters and tests driver code

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -14,6 +14,36 @@ class Santa
 	def eat_milk_and_cookies(cookie)
 		p "That was a good #{cookie}!"
 	end
+
+	def celebrate_birthday
+		@age += 1
+	end
+
+	def get_mad_at(reindeer_name)
+		reindeer_ranking.delete_if {|reindeer| reindeer == reindeer_name}
+		reindeer_ranking << reindeer_name
+	end
+
+	def change_gender(gender)
+		@gender = gender
+	end
+
+	def gender
+		@gender
+	end
+
+	def age
+		@age
+	end
+
+	def ethnicity
+		@ethnicity
+	end
+
+	def reindeer_ranking
+		@reindeer_ranking
+	end
+
 end
 
 # DRIVER CODE
@@ -24,4 +54,16 @@ claus.eat_milk_and_cookies("Chocolate Chip")
 benjamin = Santa.new("agender", "black")
 chris = Santa.new("female", "Latino")
 mickey =  Santa.new("bigender", "white")
-jan Santa.new("male", "Japanese")
+jan =  Santa.new("male", "Japanese")
+
+claus.celebrate_birthday
+p claus.age
+# should return 1
+claus.change_gender("female")
+p claus.gender
+# should return female
+p claus.ethnicity
+# should return German
+claus.get_mad_at("Dancer")
+p claus.reindeer_ranking
+# should put rudolph at the end of the list, bad rudolph
