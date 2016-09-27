ee325997aadadec4b220b364346e7706a375ee46
Sean Kung <RealNumbers@RealNumbers-MBP.local>
2016-09-27 13:20:20 -0500

    Adds yell angrily and happily methods

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,4 +1,11 @@
 # 6.4 
 module Shout
   # we'll put some methods here soon, but this code is fine for now!
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def self.yell_happily(words)
+  	words + "!!!" + " :)"
+  end
 end
