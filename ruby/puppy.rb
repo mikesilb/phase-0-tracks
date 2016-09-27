18b84d014f082c12b742810815e03f2635bc7048
Michael Silberstein <mikesilb123@gmail.com>
2016-09-27 08:49:58 -0400

    Added verbal descriptors in comments.  Release 0

---
+++ ruby/puppy.rb
@@ -0,0 +1,24 @@
+=begin
+SPECIES ------------------------
+Caninus Puppius
+
+CHARACTERISTICS ----------------
+Ears: Floppy
+Nose: Black
+Friendly: yes
+Eye count: 2
+Legs count: 4
+Fur : Always soft and cuddly
+Name: varies
+Color: varies
+Favorite Puppy Food: varies
+
+BEHAVIOR -----------------------
+Bark
+Whimper
+Jump
+Cuddle
+Kneel
+Bite
+Lay Down
+=end
