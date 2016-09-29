9036273990a879467e57c2e8e0e11efd168b42e5
Sean Kung <RealNumbers@RealNumbers-MBP.local>
2016-09-29 15:49:20 -0600

    Adds driver code for user interface and updates winner? method

--- ruby/hangman/hangman.rb
+++ ruby/hangman/hangman.rb
@@ -1,42 +1,5 @@
-=begin
-hangman criteria:
-One user can enter a word (or phrase, if you would like your game to support that),
-and another user attempts to guess the word.
-
-Guesses are limited, and the number of guesses available is related to the length 
-of the word.
-Repeated guesses do not count against the user.
-
-The guessing player receives continual feedback on the current state of the word. 
-So if the secret word is "unicorn", the user will start out seeing something like 
-"_ _ _ _ _ _ _", which would become "_ _ _ c _ _ _" after the user enters a guess of "c".
-The user should get a congratulatory message if they win, and a taunting message if they lose.
-
-Driver code will handle input and output so the methods created in Hangman class
-will take in input and return variables
-=end
-
-=begin
-Pseudocode:
-
-driver code tells user1 to pick a word
-	- the word is stored as a string and .SPLIT("") into an secret_arr.
-		- This is initialize method of Hangman
-		- initialize an answer_arr attribute with "_" for the length of secret_arr
-		- guesses varialbe = amount of guess based on length of the word
-			- guesses = secret_arr.length + 3
-dc tells user2 to start guessing letters of the word
-	- guess method of hangman
-		- takes a letter as a string
-		- index the letter with the array
-			- if found store the the guess letter in the index of the answer_arr
-					- answer_arr[(the found index)] = letter
-			- else guesses -= 1
-		- return the answer_arr
-=end
-
 class Hangman
-attr_reader :secret_arr, :answer_arr, :guesses, :guessed_arr
+attr_reader :secret_arr, :answer_arr, :guesses, :guessed_arr, :gameover
 
 	def initialize(guess_word)
 		@secret_arr = guess_word.split("")
@@ -46,8 +9,8 @@ attr_reader :secret_arr, :answer_arr, :guesses, :guessed_arr
 			@answer_arr << "_"
 		end
 		@guesses = guess_word.length + 3
-		@warning = "You've already guessed that letter. Try Again"
 		@display = ""
+		@gameover = false
 	end
 
 	def guess(letter)
@@ -62,7 +25,7 @@ attr_reader :secret_arr, :answer_arr, :guesses, :guessed_arr
 			end
 			@guesses -= 1
 		else
-			return @warning
+			return "You've already guessed that letter. Try Again"
 		end
 	end
 
@@ -72,11 +35,40 @@ attr_reader :secret_arr, :answer_arr, :guesses, :guessed_arr
 
 	def winner?
 		if @guesses != 0 && @secret_arr == @answer_arr
+			@gameover = true
 			return "You Win!"
 		elsif @guesses <= 0 
+			@gameover = true
 			return "You Lose, Loser!"
 		else
-			return false
+			return "#{guesses} remaining...choose wisely"
 		end
 	end
+
 end
+
+# DRIVER CODE
+
+puts "Welcome to a game similar to hangman!"
+puts "First player please enter a word for the second player to guess. Please use all lowercase letters"
+word = gets.chomp
+game =Hangman.new(word)
+puts "-" *30
+puts "-" *30
+puts "-" *30
+puts "-" *30
+puts "-" *30
+puts "-" *30
+puts "-" *30
+puts "Second player, guess letters, like 'a', to see if you can guess the word. You have three more chances than there are letters in the word. Please use all lowercase letters."
+while !game.gameover
+	letter = gets.chomp
+	game.guess(letter)
+	p game.display
+	p game.winner?
+end
+
+
+
+
+
