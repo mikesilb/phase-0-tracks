368d3b7b9a3656ce14b4443e7d585f12d9b38ba6
Michael Silberstein <mikesilb123@gmail.com>
2016-10-03 21:19:10 -0400

    Performs all specs of 7.1

--- js/explore.js
+++ js/explore.js
@@ -5,6 +5,7 @@
 //  a) Creates a loop that stores the following string in an array
 //  b) Counts down the loop from its final letter (i = string.length) to its first letter i>=0 using i--
 //  c) Concurrently, assign each letter to a new array variable by counting upwards from 0 to the string.length
+//  d) Perform a 'join' on that variable/array in order to group it together like a word.
 //3. In driver code, implement the function using a couple strings
 
 function reverse(string) {
@@ -25,3 +26,23 @@ console.log(test2)
 
 test3 = reverse("I am a rock.  I am an island.");
 console.log(test3)
+console.log("")
+console.log("")
+console.log("")
+
+
+if (test1=="leahciM") {
+  console.log("You nailed it!  Michael reversed is " + test1)
+}
+
+if (test1!="leahciM") {
+  console.log("Nope!!! Michael reversed is actually " + test1)
+}
+
+if (test2=="eekiM") {
+  console.log("You nailed it!  Mike reversed is " + test2)
+}
+
+if (test2 !="eekiM") {
+  console.log("Nope!!! Mike reversed is actually " + test2)
+}
