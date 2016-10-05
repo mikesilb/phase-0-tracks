0b7a2efe3ba83c82c4eaf4c9116a9cf1b084cc54
Sean Kung <RealNumbers@RealNumbers-MBP.local>
2016-10-05 16:37:23 -0500

    Adds car class

--- js/data_structures.js
+++ js/data_structures.js
@@ -8,8 +8,26 @@ colors.push("orange");
 names.push("Tom");
 
 //add color to a horses name
-var horse = {};
-
+var horses = {};
 for (i = 0; i<names.length; i++){
-	horse[names[i]] = colors[i];
+	horses[names[i]] = colors[i];
 }
+console.log(horses);
+
+//car function
+function Car(year, make, model){
+	this.year = year;
+	this.make = make;
+	this.model = model;
+	this.honk = function(){console.log("BEEPBEEP")}
+
+}
+console.log("Let's build a car");
+var newCar = new Car(2015, "Acura", "TLX");
+console.log(newCar);
+newCar.honk;
+var newerCar = new Car(2004, "Honda", "Element");
+console.log(newerCar);
+newerCar.honk;
+var dreamCar = new Car(1969, "Ford", "Mustang");
+dreamCar.honk;
