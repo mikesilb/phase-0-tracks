2eade6a24649abcb7e64cb8bb66c70003632fe3a
Michael Silberstein <mikesilb123@gmail.com>
2016-10-14 14:54:56 -0400

    Completed the categories that I plan to add

--- databases/how_am_I_feeling/how_am_I_feeling.rb
+++ databases/how_am_I_feeling/how_am_I_feeling.rb
@@ -16,6 +16,10 @@ create_feedback_table_cmd = <<-TABLE1_SQL
     time DATE,
     relationship_rate INT,
     relationship_comment VARCHAR(255),
+    career_rate INT,
+    career_comment VARCHAR(255),
+    finance_rate INT,
+    finance_comment VARCHAR(255),
     health_rate INT,
     health_comment VARCHAR(255),
     spiritual_rate INT,
@@ -35,24 +39,32 @@ db.execute(create_feedback_table_cmd)
 
 
 
-puts "What is your full name?"
+puts "1. What is your full name?"
 fullname_in = gets.chomp
 
-puts "From a 1-10, how would you rate the quality of your relationships?"
+puts "2. From a 1-10, how would you rate the quality of your relationships?"
 relationship_rate_in = gets.chomp
-puts "Would you like to elaborate about relationship stuff?"
+puts "3. Would you like to elaborate about relationship stuff?"
 relationship_comment_in = gets.chomp
-puts "From a 1-10, how would you rate your health?"
+puts "4. From a 1-10, how would you rate the way that your career is headed?"
+career_rate_in = gets.chomp
+puts "5. Would you like to elaborate about how your career is doing?"
+career_comment_in = gets.chomp
+puts "6. From a 1-10, how would you rate how well you are doing financially?"
+finance_rate_in = gets.chomp
+puts "7. Would you like to elaborate (about how you are doing financially)?"
+finance_comment_in = gets.chomp
+puts "8. From a 1-10, how would you rate your health?"
 health_rate_in = gets.chomp
-puts "Would you like to elaborate about your health?"
+puts "9. Would you like to elaborate (about your health)?"
 health_comment_in = gets.chomp
-puts "From a 1-10, how would you rate the quality of your leisure hobbies and pursuits?"
+puts "10. From a 1-10, how would you rate the quality of your leisure hobbies and pursuits?"
 leisure_rate_in = gets.chomp
-puts "Would you like to elaborate about any aspect of your leisure activities?"
+puts "11. Would you like to elaborate about any aspect of your leisure activities?"
 leisure_comment_in = gets.chomp
-puts "From a 1-10, how would you rate the quality of your spiritual activities"
+puts "12. From a 1-10, how would you rate the quality of your spiritual activities"
 spiritual_rate_in = gets.chomp
-puts "Would you like to elaborate about any aspect of your spirituality?"
+puts "13. Would you like to elaborate about any aspect of your spirituality?"
 spiritual_comment_in = gets.chomp
 
 
@@ -60,7 +72,7 @@ spiritual_comment_in = gets.chomp
 
 time = Time.now
 
-db.execute("INSERT INTO feedback (fullname, time, relationship_rate, relationship_comment, health_rate, health_comment, leisure_rate, leisure_comment, spiritual_rate, spiritual_comment) VALUES ('#{fullname_in}', '#{time}', '#{relationship_rate_in}', '#{relationship_comment_in}', '#{health_rate_in}', '#{health_comment_in}','#{leisure_rate_in}', '#{leisure_comment_in}', '#{spiritual_rate_in}', '#{spiritual_comment_in}' )")
+db.execute("INSERT INTO feedback (fullname, time, relationship_rate, relationship_comment, career_rate, career_comment, finance_rate, finance_comment, health_rate, health_comment, leisure_rate, leisure_comment, spiritual_rate, spiritual_comment) VALUES ('#{fullname_in}', '#{time}', '#{relationship_rate_in}', '#{relationship_comment_in}', '#{career_rate_in}', '#{career_comment_in}', '#{finance_rate_in}', '#{finance_comment_in}','#{health_rate_in}', '#{health_comment_in}','#{leisure_rate_in}', '#{leisure_comment_in}', '#{spiritual_rate_in}', '#{spiritual_comment_in}' )")
 
 =begin
 # add LOOOOTS of kittens!
@@ -75,12 +87,14 @@ db.execute("INSERT INTO feedback (fullname, time, relationship_rate, relationshi
 #end
 =end
 
-=begin
+
 # explore ORM by retrieving data
- kittens = db.execute("SELECT * FROM feedback")
- kittens.each do |kitten|
-  puts "#{kitten['name']} "
+ feedback_out = db.execute("SELECT fullname, time, relationship_rate, career_rate, finance_rate, health_rate, spiritual_rate, leisure_rate FROM feedback")
+
+
+ feedback_out.each do |category, rate_comment|
+  puts "#{category}, #{rate_comment} "
  end
-=end
+
 
 
