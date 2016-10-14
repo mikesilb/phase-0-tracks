6145e8f6de267628cba009f6b57c52da7c49aa4e
Michael Silberstein <mikesilb123@gmail.com>
2016-10-14 12:56:31 -0400

    Added a few more categories to the survey form

--- databases/how_am_I_feeling/how_am_I_feeling.rb
+++ databases/how_am_I_feeling/how_am_I_feeling.rb
@@ -37,17 +37,30 @@ db.execute(create_feedback_table_cmd)
 
 puts "What is your full name?"
 fullname_in = gets.chomp
-=begin
-#puts "From a 1-10, how would you rate the quality of your relationships?"
-#puts "Would you like to elaborate about relationship stuff?"
-#puts "From a 1-10, how would you rate your health?"
-#puts "Would you like to elaborate about your health?"
-=end
+
+puts "From a 1-10, how would you rate the quality of your relationships?"
+relationship_rate_in = gets.chomp
+puts "Would you like to elaborate about relationship stuff?"
+relationship_comment_in = gets.chomp
+puts "From a 1-10, how would you rate your health?"
+health_rate_in = gets.chomp
+puts "Would you like to elaborate about your health?"
+health_comment_in = gets.chomp
+puts "From a 1-10, how would you rate the quality of your leisure hobbies and pursuits?"
+leisure_rate_in = gets.chomp
+puts "Would you like to elaborate about any aspect of your leisure activities?"
+leisure_comment_in = gets.chomp
+puts "From a 1-10, how would you rate the quality of your spiritual activities"
+spiritual_rate_in = gets.chomp
+puts "Would you like to elaborate about any aspect of your spirituality?"
+spiritual_comment_in = gets.chomp
+
+
 
 
 time = Time.now
 
-db.execute("INSERT INTO feedback (fullname, time) VALUES ('#{fullname_in}', '#{time}')")
+db.execute("INSERT INTO feedback (fullname, time, relationship_rate, relationship_comment, health_rate, health_comment, leisure_rate, leisure_comment, spiritual_rate, spiritual_comment) VALUES ('#{fullname_in}', '#{time}', '#{relationship_rate_in}', '#{relationship_comment_in}', '#{health_rate_in}', '#{health_comment_in}','#{leisure_rate_in}', '#{leisure_comment_in}', '#{spiritual_rate_in}', '#{spiritual_comment_in}' )")
 
 =begin
 # add LOOOOTS of kittens!
@@ -60,11 +73,14 @@ db.execute("INSERT INTO feedback (fullname, time) VALUES ('#{fullname_in}', '#{t
 #10000.times do
 #  create_kitten(db, Faker::Name.name, 0)
 #end
-
-# explore ORM by retrieving data
-# kittens = db.execute("SELECT * FROM kittens")
-# kittens.each do |kitten|
-#  puts "#{kitten['name']} is #{kitten['age']}"
-# end
 =end
+
+=begin
+# explore ORM by retrieving data
+ kittens = db.execute("SELECT * FROM feedback")
+ kittens.each do |kitten|
+  puts "#{kitten['name']} "
+ end
+=end
+
 
